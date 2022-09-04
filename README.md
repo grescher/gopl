f8b63965b8080cd206504e6da2ac7466b9d2c1c6
Serge Chernov <gres.cher@icloud.com>
2022-09-04 14:00:47 +0300

    Add solution 1.12

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Solutions from Donovan & Kernighan's "The Go Programming Language" Book
 
-This repository contains my solutions to exercises presented in "the Go Programming Language" book, by Alan Donovan & Brian Kernighan. All solutions was compiled and tested on *macOS High Sierra (ver.10.13.6)* with *go version go1.19 darwin/amd64*.
+This repository contains my solutions to exercises presented in "the Go Programming Language" book, by Alan Donovan & Brian Kernighan. All solutions was compiled and tested with *go version go1.18.5 linux/arm64*.
 
 ## Chapters
 
-1. [Tutorial](ch01) *(except the 1.3)*
+1. [Tutorial](ch01) *(except the 1.3, 1.10, 1.11)*
---
+++ ch01/ex12/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	if len(os.Args) > 1 && os.Args[1] == "web" {
+		http.HandleFunc("/", handler)
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
+		return
+	}
+	var cycles float64 = 5.0
+	lissajous(os.Stdout, cycles)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Print(err)
+	}
+	var cycles int = 5
+	if r.Form.Has("cycles") {
+		cycles, err = strconv.Atoi(r.Form.Get("cycles"))
+		if err != nil {
+			cycles = 5
+			log.Print(err)
+		}
+	}
+	lissajous(w, float64(cycles))
+}
+
+func lissajous(out io.Writer, cycles float64) {
+	const (
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
