6bf9b029a4131b0d7b85e0dc9644c12cb7ff71ca
Serge Chernov <gres.cher@icloud.com>
2022-09-21 09:41:50 +0300

    Add comments with printed output

--- ch03/ex13/main.go
+++ ch03/ex13/main.go
@@ -18,13 +18,13 @@ const (
 )
 
 func main() {
-	fmt.Println("   kB:", comma(kB))
-	fmt.Println("   MB:", comma(MB))
-	fmt.Println("   GB:", comma(GB))
-	fmt.Println("   TB:", comma(TB))
-	fmt.Println("   PB:", comma(PB))
-	fmt.Println("   EB:", comma(EB))
-	fmt.Println("YB/ZB:", comma(YB/ZB))
+	fmt.Println("   kB:", comma(kB))    // "   kB: 1,000"
+	fmt.Println("   MB:", comma(MB))    // "   MB: 1,000,000"
+	fmt.Println("   GB:", comma(GB))    // "   GB: 1,000,000,000"
+	fmt.Println("   TB:", comma(TB))    // "   TB: 1,000,000,000,000"
+	fmt.Println("   PB:", comma(PB))    // "   PB: 1,000,000,000,000,000"
+	fmt.Println("   EB:", comma(EB))    // "   EB: 1,000,000,000,000,000,000"
+	fmt.Println("YB/ZB:", comma(YB/ZB)) // "YB/ZB: 1,000"
 }
 
 // comma inserts commas in a non-negative decimal integer string.
